e7f91d6a2dd1b334f047f822a32638ffdaade14b
Macbook <macbook@Macbooks-Luiza-Air.local>
2025-08-20 17:48:33 +0300

    Who pay the bill - update

--- who_pay_bill.py
+++ who_pay_bill.py
@@ -12,3 +12,17 @@ random_index = random.randint(0, len(name_of_people_list) - 1)
 payer = name_of_people_list[random_index]
 
 print(f"{payer} will pay the bill!")
+
+
+# or
+
+
+name_of_people = input("Enter the names of people (separated by spaces): ")
+
+# Convert the string into a list of names
+name_of_people_list = name_of_people.split()  # Splits by spaces
+
+# Pick a random person
+payer = random.choice(name_of_people_list)
+
+print(f"{payer} will pay the bill!")
